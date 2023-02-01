d679bca851d86c47e5a30914fcefc54956d1b97e
Diego Mongay <mongaydiego@gmail.com>
2023-02-01 23:25:11 +0100

    first paso veo leo commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'paso-veo-leo-web-app';
+  name = 'paso-veo-leo-web-app';
 }
